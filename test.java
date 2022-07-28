2e1132b04de62b3701838ff781cea75f85018f7c
junsuyiji <83826026+junsuyiji@users.noreply.github.com>
2022-07-28 09:15:07 +0800

    Add files via upload

---
+++ test.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class test extends Matrix {
+	public static void main(String[] args) {
+		int l = 175, b = 35;
+
+		Matrix m1 = new Matrix();
+		MatrixOperation op1 = new MatrixOperation(l, b);
+	}
+}
